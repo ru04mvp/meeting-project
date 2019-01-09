26eb3e4893561a69cd15fa0917c4a5eb677c554b
Jake <jake@jbravo.com.tw>
2019-01-09 11:51:01 +0800

    [ 前端功能 ] 增加套件 bootstrap , jquery , popper.js , vue-analytics , vue-cookie , vue-localstorage , vue-resource , vue-sweetalert2

--- web-pack/src/main.js
+++ web-pack/src/main.js
@@ -1,15 +1,42 @@
-// The Vue build version to load with the `import` command
-// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+// 引入元件
 import Vue from 'vue'
 import App from './App'
 import router from './router'
 
+// 引入自訂元件
+import VueResource from 'vue-resource'
+import VueCookie from 'vue-cookie'
+import VueLocalStorage from 'vue-localstorage'
+import VueSweetalert2 from 'vue-sweetalert2'
+import VueAnalytics from 'vue-analytics'
+
 Vue.config.productionTip = false
+
+// 設置 引用套件
+Vue.use(VueResource)
+Vue.use(VueCookie)
+Vue.use(VueLocalStorage)
+Vue.use(VueSweetalert2)
+
+/* 載入 GA 分析 */
+if (process.env.NODE_ENV === 'production') {
+  Vue.use(VueAnalytics, {
+    id: '',
+    router,
+    autoTracking: {
+      pageviewOnLoad: true
+    }
+  })
+}
 
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
   router,
   components: { App },
-  template: '<App/>'
+  template: '<App/>',
+  data: {},
+  created: function () { },
+  destroyed: function () { },
+  methods: {}
 })
--- web-pack/src/router/index.js
+++ web-pack/src/router/index.js
@@ -1,16 +1,24 @@
+// 引入元件
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
 
+// 引入頁面
+import ClientMain from './../vues/client/Main'
+
+// 引入外掛
+import './../../node_modules/bootstrap/dist/js/bootstrap.js'
+
+// 引入樣式
+import './../../node_modules/bootstrap/dist/css/bootstrap.css'
+import './../assets/css/default.css'
+
+// 使用元件
 Vue.use(Router)
 
 export default new Router({
   mode: 'history',
   routes: [
-    {
-      path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
-    }
+    // 首頁
+    { path: '/', name: 'page-index-main', component: ClientMain }
   ]
 })
