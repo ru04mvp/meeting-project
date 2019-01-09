70cce2a4a0cb1d6719af35ff510e378107ef1a8a
Jake <jake@jbravo.com.tw>
2019-01-09 11:51:20 +0800

    [ 前端架構 ] 調整系統架構

--- web-pack/src/App.vue
+++ web-pack/src/App.vue
@@ -1,8 +1,6 @@
-<template>
-  <div id="app">
-    <img src="./assets/logo.png">
-    <router-view/>
-  </div>
+<template lang="jade">
+  #app
+    router-view
 </template>
 
 <script>
@@ -12,12 +10,4 @@ export default {
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
